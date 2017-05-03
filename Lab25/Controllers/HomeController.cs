5d45bd0fd488ea6bb90e31184d40ca9d2a87b45d
mspisich <mike.spisich@gmail.com>
2017-05-03 09:45:48 -0400

    First commit.

---
+++ Lab25/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab25.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            bool isLoggedIn = System.Web.HttpContext.Current.User.Identity.IsAuthenticated;
+            if (isLoggedIn)
+            {
+                return RedirectToAction("Welcome", "Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+
+        [Authorize]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+
+        [Authorize]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            ViewBag.Name = User.Identity.Name;
+
+            return View();
+        }
+    }
+}
